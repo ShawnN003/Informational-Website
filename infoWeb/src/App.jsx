3d544613ce63a7dbb05be11691f15ae535c215b3
ShawnN003 <nguru.shawn@student.greenriver.edu>
2025-10-02 15:51:25 -0500

    update react page

--- infoWeb/src/App.jsx
+++ infoWeb/src/App.jsx
@@ -9,25 +9,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="" target="_blank">
+          <img src={viteLogo}  alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Sci-Fi Cinema</h1>
+
+      <h3>2000s</h3>
+      <h3>2000s</h3>
     </>
   )
 }
