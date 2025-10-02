0ae29a5a04c67d054b230663ed49c42ca1ccde09
ShawnN003 <nguru.shawn@student.greenriver.edu>
2025-10-02 16:07:19 -0500

    information of the first movie

--- infoWeb/src/App.jsx
+++ infoWeb/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="" target="_blank">
+        <a href="src={warOfWorlds}" target="_blank">
           <img src={viteLogo}  alt="Vite logo" />
         </a>
         <a href="" target="_blank">
@@ -18,7 +18,13 @@ function App() {
       </div>
       <h1>Sci-Fi Cinema</h1>
 
-      <h3>2000s</h3>
+      <h3>War of worlds</h3>
+      <ul>
+        <li>Based from a novel in 1897</li>
+        <li>One of the earliest alien invasion movies</li>
+        <li>Has various adaptations</li>
+
+      </ul>
       <h3>2000s</h3>
     </>
   )
