0cfaf4a0af21b61efa9299ac66bbf98e0b5a8563
ShawnN003 <nguru.shawn@student.greenriver.edu>
2025-10-02 16:25:44 -0500

    Image working

--- infoWeb/src/App.jsx
+++ infoWeb/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import firstMovie from './images/warOfworlds.webp'
+
 import './App.css'
 
 function App() {
@@ -9,13 +11,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="src={warOfWorlds}" target="_blank">
-          <img src={viteLogo}  alt="Vite logo" />
+        <a href="src/images/warOfworlds.webp" target="_blank">
+          <img src={firstMovie}  alt="Vite logo"/>
         </a>
         <a href="" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
+      
       <h1>Sci-Fi Cinema</h1>
 
       <h3>War of worlds</h3>
@@ -23,9 +26,13 @@ function App() {
         <li>Based from a novel in 1897</li>
         <li>One of the earliest alien invasion movies</li>
         <li>Has various adaptations</li>
-
       </ul>
-      <h3>2000s</h3>
+      <h3>Interstellar</h3>
+      <ul>
+        <li>They had an actual physicist to simulate how the the black hole would effect the characters</li>
+        <li>The work was accurate enough that it inspired a new scientific discovery about how light behaves</li>
+        <li>Has various adaptations</li>
+      </ul>
     </>
   )
 }
